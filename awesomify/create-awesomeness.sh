aaadb199504644a0a295ddd4d7cc5c0b50066950
Daniel Llewellyn <daniel@bowlhat.net>
2019-01-17 12:40:26 +0000

    initial commit

---
+++ awesomify/create-awesomeness.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+urldecode() { : "${*//+/ }"; echo -e "${_//%/\\x}"; }
+
+NAME="$Http_Query"
+if [ -z "$NAME" ]; then
+    while read NAME; do
+        if [ -n "$NAME" ]; then
+            break
+        fi
+    done
+fi
+
+[ -n "$NAME" ] && NAME="$(urldecode "$NAME")"
+
+if [ -z "$NAME" ]; then
+    echo "You need to specify an awesome name"
+    exit 1
+fi
+
+TMPWAV="$(mktemp -u).wav"
+
+mimic -t "$NAME" -o /dev/stdout | sox -t wav - -c 2 -r 44.1k "$TMPWAV"
+
+CLIPS=()
+for clip in "$(dirname $0)"/clip*.wav; do
+    CLIPS+=("$clip" "$TMPWAV")
+done
+unset 'CLIPS[${#CLIPS[@]}-1]'
+
+sox "${CLIPS[@]}" -t wav - \
+    | opusenc --bitrate 24 --downmix-mono - - 2> /dev/null
+
+rm -f "$TMPWAV"
